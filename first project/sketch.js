2fbc74c773c4dfb146ce60e50e907a70d5697bdc
spngbob <54913045+spngbob@users.noreply.github.com>
2019-09-06 14:12:04 -0600

    added colour

--- first project/sketch.js
+++ first project/sketch.js
@@ -1,17 +1,35 @@
-// Project Title
-// Your Name
+// Project
+// Jenna Doucette
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let redAmount = 0;
+let redChangeAmount = 1;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-}
+  background(0, 0, 0);
+  }
 
 function draw() {
-  background(220);
-  fill(0);
+  fill(redAmount, 255, 0);
+  noStroke();
   ellipse(mouseX, mouseY, 50, 50);
-}
+ 
+  redAmount += redChangeAmount; 
+
+  if (redAmount >= 255) { 
+    redChangeAmount *= -1;
+  }
+
+  if (redAmount <= 0) {
+   redChangeAmount *= -1;
+  }
+
+  console.log(redAmount);
+  }
+
+
