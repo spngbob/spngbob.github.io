d220bf2949c17582e19d9b636fc73216cf9c6258
spngbob <54913045+spngbob@users.noreply.github.com>
2019-09-12 12:23:20 -0600

    started a new projet

---
+++ interactive project/sketch.js
@@ -0,0 +1,31 @@
+// Interactive Project
+// Jenna Doucette
+// September 12, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let redAmount = 0;
+let redChangeAmount = 1;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0, 0, 0);
+  }
+
+function draw() {
+  // fill(redAmount, 0, 255);
+  // noStroke();
+  // ellipse(mouseX, mouseY, 50, 50);
+ 
+  // redAmount += redChangeAmount; 
+
+  // if (redAmount >= 255) { 
+  //   redChangeAmount *= -1;
+  // }
+
+  // console.log(redAmount);
+  // }
+
+
