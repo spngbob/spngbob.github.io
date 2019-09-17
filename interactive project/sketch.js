9e927d195d115adcfefb3981ae344a7252f10c3f
spngbob <54913045+spngbob@users.noreply.github.com>
2019-09-17 14:09:38 -0600

    made a really bad two player tic tac toe game that doesn't keep track of score or anything

--- interactive project/sketch.js
+++ interactive project/sketch.js
@@ -5,22 +5,35 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x;
+let y;
+let my;
+let mx;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   stroke(0);
   fill(0);
-  }
-
-function draw() {
-  line(30, 20, 85, 20);
-  if (keyIsPressed){
-    if (keyCode === UP_ARROW){
-      red = random(0, 255)
-      green = random(0, 255)
-      blue = random(0, 255)
-    }
-  }
+  x = windowWidth/2;
+  y = windowHeight/2;
 }
 
+function draw() {
+  line(x-350, y+100, x+350, y+100 ); // horizontal line
+  line(x-350, y-150, x+350, y-150 ); // horizontal line
+  line(x-125, y-350, x-125, y+300 ); // vertical line
+  line(x + 100, y-350, x+100, y+300 ); // vertical line
+  }
+
+  function keyPressed() {
+    if (keyCode === 88){  
+      line(mouseX-50, mouseY - 50, mouseX +50, mouseY +50);
+      line(mouseX + 50, mouseY - 50, mouseX -50 , mouseY + 50);}
+    
+    else if (keyCode === 79){
+      fill("white")
+      circle(mouseX, mouseY, 100)}
+
+    else if (keyCode === 32)  {}
+  }
