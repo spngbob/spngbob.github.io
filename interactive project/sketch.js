a811a0c99f9c20b2e0c1040db85a99ba78fc33f2
spngbob <54913045+spngbob@users.noreply.github.com>
2019-09-19 14:13:59 -0600

    making it work
    made it work for tic tac toe, trying to add function where it clears the playing fieled when DELETE is pressed. not working currently.

--- interactive project/sketch.js
+++ interactive project/sketch.js
@@ -11,14 +11,10 @@ let my;
 let mx;
 let mode;
 let counter;
-counter = 0
+counter  = 0;
+mode = "turnX";
 
-function setUpLines(){
-  line(x-350, y+100, x+350, y+100 ); // horizontal line
-  line(x-350, y-125, x+350, y-125 ); // horizontal line
-  line(x-125, y-350, x-125, y+300 ); // vertical line
-  line(x + 100, y-350, x+100, y+300 ); // vertical line
-}
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -29,24 +25,35 @@ function setup() {
   y = windowHeight/2;
 }
 
+function setUpLines(){
+  line(x-350, y+100, x+350, y+100 ); // horizontal line
+  line(x-350, y-125, x+350, y-125 ); // horizontal line
+  line(x-125, y-350, x-125, y+300 ); // vertical line
+  line(x + 100, y-350, x+100, y+300 ); // vertical line
+}
+
 function draw() {
   setUpLines()
-  mode = "turnX"
-    if (mouseIsPressed === true){  
-      if (counter < 9){
-        console.log("turnX")
-        if (mode === "turnX"){
-          line(mouseX-50, mouseY - 50, mouseX +50, mouseY +50);
-          line(mouseX + 50, mouseY - 50, mouseX -50 , mouseY + 50);
-          counter = counter + 1;
-          mode = "turnY";
-        }
-        else if (mode === "turnY"){
-          fill("white");
-          circle(mouseX, mouseY, 100);
-          counter = counter + 1;
-          mode = "turnX";                                                                          
-        }
-      }
+}
+
+
+function mouseReleased(){
+  if (counter < 9){  
+    if (mode === "turnX"){
+      line(mouseX-50, mouseY - 50, mouseX +50, mouseY +50);
+      line(mouseX + 50, mouseY - 50, mouseX -50 , mouseY + 50);
+      counter++;
+      mode = "turnY";
     }
-  }
+    else if (mode === "turnY"){
+      fill("white");
+      circle(mouseX, mouseY, 100);
+      counter++;
+      mode = "turnX";}
+      if (keyIsPressed === true){
+        if (keyCode === DELETE){
+          clear();
+          mode = "turnX";}
+    }
+}
+}
