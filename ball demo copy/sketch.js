7c6eb21477c91e63153d218e35de7c5d507cb079
spngbob <54913045+spngbob@users.noreply.github.com>
2019-10-15 14:00:30 -0600

    made a cannon that shoots balls

---
+++ ball demo copy/sketch.js
@@ -0,0 +1,65 @@
+// CANNON BALL DEMO NOT BALL DEMO IT WONT LET ME RENAME
+
+
+let cannonX;
+let cannonY;
+let cannonLength;
+let cannonWidth;
+let cannonAngle;
+let bullets = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cannonX = windowWidth/2 ;
+  cannonY = windowHeight/2;
+  cannonWidth = 50;
+  cannonLength = 125;
+  cannonAngle = 0;
+}
+
+
+
+function draw() { 
+  background(100);
+  displayCannon();
+  noStroke();
+  updateBullets();
+}
+
+
+
+function displayCannon() {
+  push(); // save the transformation matrix
+  translate(cannonX, cannonY);
+  cannonAngle = atan2(mouseY - cannonY, mouseX - cannonX);
+  rotate(cannonAngle);
+  rect(0, -cannonWidth/2, cannonLength, cannonWidth);
+  circle(0,0,cannonWidth);
+  pop(); // reload the old transformation matrix
+
+}
+
+
+function mouseClicked() {
+  fire();
+}
+
+function fire(){
+  let thisBullet = {
+    x: cannonX, 
+    y: cannonY,
+    radius: cannonWidth - 2,
+    angle: cannonAngle,
+    speed: 50 };
+  bullets.push(thisBullet); 
+}
+
+
+function updateBullets() {
+  for (let thisBullet of bullets){
+    thisBullet.x  +=  thisBullet.speed * cos(thisBullet.angle);
+    thisBullet.y += thisBullet.speed * sin(thisBullet.angle);
+    circle(thisBullet.x, thisBullet.y, thisBullet.radius);
+
+  }
+}
--- state variable proj/sketch.js
+++ state variable proj/sketch.js
@@ -1,9 +1,8 @@
-// State Variable Project  - ping pong but lonely and also BETTER than before
+// State Variable Project  - colourful single player ping pong
 // Jenna Doucette
 // September 12, 2019 
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Extra for Experts: 
+
 
 
 
@@ -15,9 +14,6 @@ let dy;
 let r = 255;
 let g = 255;
 let b = 255;
-
-let filler = ["red", "green", "blue", "pink", "orange", "yellow", "red", "green", "blue", "pink", "purple"]
-
 let score = 1;
 let myScore = 0;
 
@@ -25,9 +21,6 @@ let paddleHeight = 10;
 let paddleWidth = 75;
 let paddleX = (windowWidth-paddleWidth) / 2;
 
-//function changeColour(nums){
-// WORKD ON HTHIS ASDFJAHSDLKFJHSADJF HALDSKFJHA LSDJ HFLAKJDHFALKJDS FAKSLJDFHASDJKFHALKJDFHALKJDSHFLKJSADHFALKSJDHFLAKJSDHFLKAJSDHFKJADSHF
-//}
 
 
 
@@ -35,13 +28,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   x = width/2;
   y = height/2; 
-  dx = random(-15, 15);      //setting up speed and 
-  dy = random(-15, 15);
-  r = random(0, 255);
-  g = random(0, 255);
-  b = random(0, 255);
+  dx = random(-10, 10);      //setting up speed and 
+  dy = random(-10, 10);
 }
-
 
 
 
@@ -70,38 +59,32 @@ function bouncyBoi(){
     dy *= -1;
     myScore = myScore+1;
     textSize(40);
+    noStroke();
+    fill('white');
     textFont('Georgia');
-    fill(r, g, b);
-    text('score: ' + myScore, 200, 200)
 
+    text('score: ' + myScore, 200, 200)
+  }
 
   
-  if ( y > windowHeight - 90){  // sets up fail screen ! 
+  if ( y > windowHeight - radius){  // sets up fail screen ! 
     clear();
     background(220, 20, 60);
     textSize(40);
-    fill("white")
-    noStroke();
     textFont('Georgia');
     text('YOU FAILED', windowWidth/2, windowHeight/2, 500, 500);
     text('press space to continue', windowWidth/2-100, windowHeight/2+200, 500, 500)
     text('you lost' + score + " game(s)", 200, 200)
-    dy = windowHeight;
     
     
     
     if (keyIsPressed){ // clears screen and resets the paddle, score = 0, and restarts the game
-      if (keyCode === 32){ // press space and you clear
+      if (keyCode === 32){
         background(0);
         x = width/2;
         y = height/2;
         dx = random(-10, 10);
         dy = random(-10, 10);
-        noStroke();
-        r = random(0, 255);
-        g = random(0, 255);
-        b = random(0, 255);
-        fill(r, g, b);
         bouncyBoi();
         paddle();
         clear();
@@ -109,7 +92,7 @@ function bouncyBoi(){
         myScore = 0;
   }
 } 
-}
+
 
 
 
@@ -119,5 +102,4 @@ function draw() {   // moving around + bouncing
   circle(x, y, radius);
   bouncyBoi();
   paddle();
-
 }
