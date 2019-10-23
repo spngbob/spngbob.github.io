0b8d98f12f345c817a229976660e745c80339640
spngbob <54913045+spngbob@users.noreply.github.com>
2019-10-23 13:45:19 -0600

    ficed
    just

---
+++ template/sketch.js
@@ -0,0 +1,15 @@
+// Project
+// Jenna Doucette
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+}
