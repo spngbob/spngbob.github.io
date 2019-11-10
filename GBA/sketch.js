8f830a42805c17ff14a84ecb39fe9a09cd6dc0a9
Jenna  Doucette <jennadoucette@gmail.com>
2019-11-10 13:18:46 -0600

    cut everything away to basic

--- GBA/sketch.js
+++ GBA/sketch.js
@@ -11,13 +11,7 @@ let grid = [];
 let rows = 3;
 let cols = 3;
 let cellSize;
-let redscore = 0;
-let bluescore = 0;
-let tiescore = 0;
-let clicks = 9;
-let wowloser = 0; 
 
-let state = "menu"
 
 
 function setup() {
@@ -27,24 +21,7 @@ function setup() {
 
 function draw() {
   background(220);
-  if (state === "menu"){
-    showMenu();
-  }
-  if (state === "red"){
-    displayGrid(grid);
-  }  
-  if (state === "blu"){
-    displayGrid(grid);
-  }
-  else if (state === "rin"){
-    RedwinScreen();
-  }
-  else if (state === "bin"){
-    BluewinScreen();
-  }
-  else if (state === "tie"){
-    tieScreen(); 
-  }
+  displayGrid(grid);
 }
 
 function displayGrid(theGrid){ 
@@ -79,70 +56,3 @@ function createArray(cols, rows){
     }
   return someArray;
 }
-
-function RedwinScreen() {
-  background(255);
-  fill(0);
-  textSize(85);
-  if (windowWidth > windowHeight) {
-    textAlign(windowHeight/2, windowHeight/2);
-    text("RED WIN!", windowHeight/2, windowHeight/2);
-  }
-  else {
-    textAlign(windowWidth/2, windowWidth/2);
-    text("RED WIN!", windowWidth/2, windowWidth/2);  }
-}
-
-function BluewinScreen() {
-  background(255);
-  fill(0);
-  textSize(85);
-  if (windowWidth > windowHeight) {
-    textAlign(windowHeight/2, windowHeight/2);
-    text("BLUE WIN!", windowHeight/2, windowHeight/2);
-  }
-  else {
-    textAlign(windowWidth/2, windowWidth/2);
-    text("BLUE WIN!", windowWidth/2, windowWidth/2);  }
-}
-
-function tieScreen() {
-  background(255);
-  fill(0);
-  textSize(85);
-  if (windowWidth > windowHeight) {
-    textAlign(windowHeight/2, windowHeight/2);
-    text("WOW NEITHER OF YOU WIN!", windowHeight/2, windowHeight/2);
-  }
-  else {
-    textAlign(windowWidth/2, windowWidth/2);
-    text("WOW NEITHER OF YOU WIN!", windowWidth/2, windowWidth/2);  }
-}
-
-
-function showMenu() {
-  // shows play button 
-  if (windowWidth > windowHeight) {
-    fill(255, 105, 105);
-    rect(windowHeight/2-(windowHeight/4), windowHeight/2 - (windowHeight/4), windowHeight/2, windowHeight/5);
-    textSize(windowHeight*0.15);
-    fill(0);
-    text("PLAY", windowHeight/2, windowHeight/2);
-  }
-  else {
-    fill(255, 105, 105);
-    rect(windowWidth/2-(windowWidth/4), windowWidth/2-100 -(windowWidth/4), windowWidth/2, windowWidth/5);
-    textSize(windowWidth*0.15);
-    fill(0);
-    text("PLAY", windowWidth/2, windowWidth/2);
-}
-}
-
-// function isButtonClicked() {
-//   if (mouseIsPressed) {
-//     // check if the play button is pressed
-//     if (mouseX > width/2 - 200 && mouseX < width/2 + 200 && mouseY > height/2 - 100 - 75 && mouseY < height/2 - 100 + 75) {
-//         state = "red";
-//     }
-//   }
-// 
